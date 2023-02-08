03dab2b47465a11c2ad65ef297c2233fe7e0f7fd
Marshmillo <jcamilodz@gmail.com>
2023-02-08 18:06:10 -0500

    first commit

---
+++ test.py
@@ -0,0 +1,14 @@
+from app import app
+
+def test1():
+    response = app.test_client.get('/')
+    assert response.status_code==200
+
+def test2():
+    response = app.test_client.get('/base')
+    assert response.status_code==200
+
+def test3():
+    response = app.test_client.get('/base')
+    assert b"My Daily checklist" in response.data
+    assert b"Added Tasks" in response.data
